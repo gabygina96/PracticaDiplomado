5f311ccebbccfabd76608dfa2a1acbd97842b697
gabygina96 <48263934+gabygina96@users.noreply.github.com>
2020-02-07 20:05:38 -0600

    Add files via upload

---
+++ Assets/Scripts/VRNodesRig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRNodesRig : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    
+    void Update()
+    {
+        head.transform.SetPositionAndRotation(InputTracking.GetLocalPosition(XRNode.Head), InputTracking.GetLocalRotation(XRNode.Head));
+        leftHand.transform.SetPositionAndRotation(InputTracking.GetLocalPosition(XRNode.LeftHand), InputTracking.GetLocalRotation(XRNode.LeftHand));
+        rightHand.transform.SetPositionAndRotation(InputTracking.GetLocalPosition(XRNode.RightHand), InputTracking.GetLocalRotation(XRNode.RightHand));
+    }
+}
