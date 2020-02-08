0a0317c0fe4b9a0f2e81002f7a466bb91b95fbd4
dakedekaane <servantarcherubw@gmail.com>
2020-02-08 09:29:38 -0600

    Added shooter functions.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.Oculus;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float forceBullet;
+    public bool isActive = false;
+    private GameObject tmpBullet;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && isActive){
+            tmpBullet = Instantiate(bulletPrefab,transform.position,Quaternion.identity);
+            tmpBullet.transform.up = transform.forward;
+            tmpBullet.GetComponent<Rigidbody>().AddForce(transform.forward * forceBullet, ForceMode.Impulse);
+        }
+    }
+}
