f03be4506484e431f198719a8b558ff793c67b3d
dakedekaane <servantarcherubw@gmail.com>
2020-02-08 09:57:02 -0600

    Added Oculus Input

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.XR.Oculus;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Shooter : MonoBehaviour
 {
+    public XRGrabInteractable gun;
+    
     public GameObject bulletPrefab;
     public float forceBullet;
     public bool isActive = false;
     private GameObject tmpBullet;
 
+    void Start() {
+        gun.onActivate.AddListener(TriggerPulled);
+    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetButtonDown("Fire1") && isActive){
+
+    void TriggerPulled(XRBaseInteractor obj) {
+        if(isActive){
             tmpBullet = Instantiate(bulletPrefab,transform.position,Quaternion.identity);
             tmpBullet.transform.up = transform.forward;
             tmpBullet.GetComponent<Rigidbody>().AddForce(transform.forward * forceBullet, ForceMode.Impulse);
